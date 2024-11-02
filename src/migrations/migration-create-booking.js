8475b179a1f3b70107e9fa1e055dc30898ea88e0
22424022tinh <khangtruongbuu@gmail.com>
2024-11-02 21:36:07 +0700

    api create booking

--- src/migrations/migration-create-booking.js
+++ src/migrations/migration-create-booking.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       statusId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       doctorId: {
         type: Sequelize.INTEGER
